2588a4cbd8fa920e35dedfc9d1c1c4f685cf526e
Adam Johnson <me@adamj.eu>
2021-08-12 09:55:17 +0100

    Add type hints

--- src/pytest_reverse/__init__.py
+++ src/pytest_reverse/__init__.py
@@ -1,7 +1,12 @@
+from typing import Generator, List
+
 import pytest
+from _pytest.config import Config
+from _pytest.config.argparsing import Parser
+from _pytest.nodes import Item
 
 
-def pytest_addoption(parser):
+def pytest_addoption(parser: Parser) -> None:
     group = parser.getgroup("reverse", "Reverse test order")
     group._addoption(
         "--reverse",
@@ -12,7 +17,9 @@ def pytest_addoption(parser):
 
 
 @pytest.hookimpl(hookwrapper=True, tryfirst=True)
-def pytest_collection_modifyitems(session, config, items):
+def pytest_collection_modifyitems(
+    config: Config, items: List[Item]
+) -> Generator[None, None, None]:
     if config.getoption("reverse"):
         items[:] = items[::-1]
 
